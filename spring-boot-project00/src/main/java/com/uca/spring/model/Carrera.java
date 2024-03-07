56933ad7ca79a56ca9ec14c1fe7ea3811f88e1bb
andresderas <00362718@uca.edu.sv>
2024-03-07 01:11:59 -0600

    Add: Carrera model

---
+++ spring-boot-project00/src/main/java/com/uca/spring/model/Carrera.java
@@ -0,0 +1,38 @@
+package com.uca.spring.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+	@Table(name = "Carrera")
+
+public class Carrera {
+	
+	@Id
+	@Column(name="id_carrera")
+	Integer idCarrera;
+	
+	@Column(name="uv_aprobadas")
+	Integer uvAprobadas;
+	
+	@Column(name="materias_aprobadas")
+	String materiasAprobadas;
+	
+	@Column(name="cantidad_materias_posibles")
+	Integer cantidadMateriasPosibles;
+	
+	@Column(name="materias_posibles")
+	String materiasPosibles;
+	
+	@Column(name="cantidad_actividades_extra")
+	Integer cantidadActividadesExtracurriculares;
+	
+	@Column(name="nota_aprobada")
+	String notaAprobada;
+	
+}
